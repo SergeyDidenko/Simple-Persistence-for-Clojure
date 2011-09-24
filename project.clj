0bd80899577c4f99d84a223d0aa8b608629f8653
Sergey Didenko <sergey.didenko@gmail.com>
2011-09-24 11:53:14 +0300

    Changed dependency to Clojure 1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject persister "1.0.1"
   :description "Simple Persistence for Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :local-repo-classpath true)
